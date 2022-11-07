0f719a9d4e98c0f3b7cc5a94a22c781c39f5c2a6
matthew-lui <107003143+matthew-lui@users.noreply.github.com>
2022-11-07 13:03:26 -0500

    testing

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Header from "./Header";
 import ToyForm from "./ToyForm";
 import ToyContainer from "./ToyContainer";
 
+const API = "http://localhost:3001/toys"
+
 function App() {
+  const [toys, setToys] = useState("")
   const [showForm, setShowForm] = useState(false);
+
+  useEffect(() =>{
+    fetch(API)
+    .then(response => response.json())
+    .then(toyData => setToys(toyData))
+  },[])
 
   function handleClick() {
     setShowForm((showForm) => !showForm);
+   
   }
 
   return (
@@ -18,7 +28,7 @@ function App() {
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
-      <ToyContainer />
+      <ToyContainer toys={toys} />
     </>
   );
 }
